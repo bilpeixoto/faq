b00b0befa8b9dccf1c99ccf2585e00f2b667faec
bilpeixoto <g4briel.soarespeixoto@gmail.com>
2021-04-08 13:28:01 -0300

    Componente EscolhaCategoria Finalizado

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <div class="box">
+      <EscolhaCategoria> </EscolhaCategoria>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import EscolhaCategoria from '@/components/EscolhaCategoria.vue'
 
 export default {
-  name: "App",
   components: {
-    HelloWorld,
-  },
-};
+    EscolhaCategoria
+  }
+}
 </script>
 
 <style>
+h1 {
+  font-size: 20px;
+}
+h3 {
+  font-size: 13px;
+  font-weight: 400;
+}
+.container {
+  width: 100vw;
+  height: 100vh;
+  display: grid;
+  justify-content: center;
+  align-items: center;
+}
+
+.box {
+  width: 305px;
+  height: 457px;
+  background-image: linear-gradient(180deg, #3f4452, #26282c);
+  border-radius: 15px;
+  display: grid;
+  padding: 2rem 0.375rem;
+  padding-bottom: 0.875rem;
+}
+
 * {
   box-sizing: border-box;
   padding: 0;
   margin: 0;
+  color: #f5f6f8;
+  list-style: none;
 }
-
 body {
   font-family: Lato;
 }
-
 a {
   color: #22e0db;
   text-decoration: none;
 }
-
 @font-face {
   font-family: Lato;
-  src: url("~@/assets/fonts/Lato-Regular.ttf");
+  src: url('~@/assets/fonts/Lato-Regular.ttf');
   font-weight: 400;
   font-display: block;
 }
-
 @font-face {
   font-family: Lato;
-  src: url("~@/assets/fonts/Lato-Bold.ttf");
+  src: url('~@/assets/fonts/Lato-Bold.ttf');
   font-weight: 700;
   font-display: block;
 }
---
+++ src/components/EscolhaCategoria.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="heading">
+    <div class="heading-image">
+      <img src="@/assets/images/astronaut.svg" />
+    </div>
+    <div class="heading-text">
+      <h1>Perguntas Frequentes</h1>
+      <h3>Escolha a categoria desejada</h3>
+    </div>
+  </div>
+  <ul class="categories">
+    <li
+      v-for="category in faqCategories"
+      :key="category.id"
+      class="category-item"
+    >
+      <img :src="require(`@/assets/images/${category.icon}`)" />
+      {{ category.title }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import { faqCategories } from '@/utils/db.json'
+
+export default {
+  data() {
+    return {
+      faqCategories
+    }
+  }
+}
+</script>
+
+<style scoped>
+.heading {
+  display: grid;
+  padding: 0 1.5em;
+  padding-bottom: 1em;
+}
+
+.heading-text {
+  display: grid;
+  grid-template-rows: auto 1fr;
+  grid-gap: 0.5rem;
+}
+.categories {
+  display: grid;
+  padding-bottom: 1.2rem;
+}
+
+.category-item {
+  display: grid;
+  grid-template-columns: auto 1fr;
+  grid-gap: 1.7rem;
+  cursor: pointer;
+  transition: background-color 0.5s ease-in-out;
+  align-items: center;
+  padding: 0.7em 1.5em;
+  border-radius: 30px;
+}
+
+.category-item:hover {
+  background-color: #3f4452;
+}
+
+.category-item img {
+  width: 24px;
+}
+</style>
